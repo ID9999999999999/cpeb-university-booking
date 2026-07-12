c2e69e31fea17eb55aa802a8f537b633af5477b4
Yasser Idbouzkri <idbouzkriy@gmail.com>
2026-07-12 20:09:31 +0800

    Initial public release of CPEB university booking system

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { EquipmentModule } from './equipment/equipment.module';
+import { MaintenanceModule } from './maintenance/maintenance.module';
+import { RepairTicketsModule } from './repair-tickets/repair-tickets.module';
+import { AuditLogsModule } from './audit-logs/audit-logs.module';
+
+@Module({
+  imports: [
+    PrismaModule,
+    BookingsModule,
+    EquipmentModule,
+    MaintenanceModule,
+    RepairTicketsModule,
+    AuditLogsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
