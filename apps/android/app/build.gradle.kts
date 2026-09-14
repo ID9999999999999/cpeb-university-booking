f565e71c977fe01906f20e8bac60965c79adc132
陈易 <idbouzkriy@gmail.com>
2026-09-14 15:56:09 +0800

    Bump Android release to v1.4.0

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -58,8 +58,8 @@ android {
         applicationId = "com.yasser.ub"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.3.0"
+        versionCode = 6
+        versionName = "1.4.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
